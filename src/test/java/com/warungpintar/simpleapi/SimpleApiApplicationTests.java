6bfab05c8cbacffa7ec47f46ac99d9cd6b1d871a
wendy <wendy.wendy@dimo.co.id>
2020-05-07 19:06:21 +0700

    initialize project.

---
+++ src/test/java/com/warungpintar/simpleapi/SimpleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warungpintar.simpleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
